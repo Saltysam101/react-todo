815bc3d81ef9baea6f0fe51ed99e19be557429c1
Sam Zudell <s.zudell98@gmail.com>
2023-05-08 11:40:33 -0400

    styled welcome page

--- src/client/src/pages/welcome.page.jsx
+++ src/client/src/pages/welcome.page.jsx
@@ -3,11 +3,17 @@ import { Link } from 'react-router-dom';
 
 export default function WelcomePage() {
   return (
-    <div>
-      <h1 className="">Welcome to The To-Do site</h1>
-      <p>Please either setup an account or login</p>
-      <Link to="/register">Sign Up</Link>
-      <Link to="/login">Login</Link>
+    <div className="flex flex-col items-center justify-evenly h-screen">
+      <header className="text-center">
+        <h1 className="text-2xl ">Welcome to The To-Do site</h1>
+        <p className="text-md md:text-sm">Please either setup an account or login</p>
+      </header>
+      <section className=" w-4/5 flex flex-row items-center justify-evenly">
+      <Link 
+      className="bg-blue-500 rounded border-2 p-1 text-white hover:bg-white hover:text-blue-500 hover:border-blue-500" to="/register">Sign Up</Link>
+      <Link 
+      className="border-2 border-blue-500 rounded py-1 px-2 text-blue-500 hover:bg-blue-500 hover:text-white" to="/login">Login</Link>
+      </section>
     </div>
   )
 }
