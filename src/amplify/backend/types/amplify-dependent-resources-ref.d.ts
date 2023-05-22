edfff46293bffb293ab808167324fdc06f6355e8
Sam Zudell <s.zudell98@gmail.com>
2023-05-22 09:49:57 -0400

    added graphql api and schema

--- src/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ src/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,9 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "reacttodo": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
